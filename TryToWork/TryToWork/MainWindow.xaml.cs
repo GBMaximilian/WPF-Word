8c2a59eb87faf87d8803b3cf47355d70cd766349
MSI\straa <straaus2323@gmail.com>
2018-09-10 20:13:10 +0300

    пререведено с WF на WPF, добавлена работа с колонтикулом

---
+++ TryToWork/TryToWork/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Word = Microsoft.Office.Interop.Word;
+using Microsoft.Win32;
+
+namespace TryToWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = OPEN();
+
+
+            var word_app = new Word.Application();
+            word_app.Visible = false;//отображение ворда во время работы кода
+
+            var wordDoc = word_app.Documents.Open(filename);
+
+            ReplaceWordStub("{}", textBox.Text, wordDoc);
+            SAVE(wordDoc, word_app);
+        }
+
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var word_app = new Word.Application
+            {
+                Visible = false
+            };
+
+            // Создаем документ Word.
+            object missing = Type.Missing;
+
+            var word_doc = word_app.Documents.Add();
+
+            // Создаем абзац заголовка.
+            var para = word_doc.Paragraphs.Add(ref missing);
+
+            object style_name = "Заголовок 1";
+            para.Range.set_Style(ref style_name);
+            para.Range.Text += "Кривая хризантемы";
+            para.Range.InsertParagraphAfter();
+
+            
+
+            //Добавление верхнего колонтитула
+            foreach (Microsoft.Office.Interop.Word.Section section in word_doc.Sections)
+            {
+                Microsoft.Office.Interop.Word.Range headerRange =
+                section.Headers[Microsoft.Office.Interop.Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                headerRange.Fields.Add(headerRange, Microsoft.Office.Interop.Word.WdFieldType.wdFieldPage);
+                headerRange.ParagraphFormat.Alignment =
+                Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                headerRange.Font.ColorIndex = Microsoft.Office.Interop.Word.WdColorIndex.wdBlue;
+                headerRange.Font.Size = 10;
+                headerRange.Text = "Верхний колонтитул" + Environment.NewLine + "www.CSharpCoderR.com";
+            }
+            
+
+            para.Range.Font.Size = 13;
+            para.Range.Font.Bold = -1;
+
+            richTextBox.SelectAll();
+            string myText = richTextBox.Selection.Text;
+
+            para.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            para.Range.Text += richTextBox.Selection.Text;
+            para.Range.InsertParagraphAfter();
+
+            para.Range.Font.Italic = -1;
+            para.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            para.Range.Text += richTextBox.Selection.Text;
+            para.Range.InsertParagraphAfter();
+
+
+
+            para.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphRight;
+            para.Range.Text += richTextBox.Selection.Text;
+            para.Range.InsertParagraphAfter();
+
+
+            //richTextBox1.Selection.Text = para.Range.Text;
+
+            //Set myRange = ActiveDocument.Words(1)
+            //para.Range.Words(1) = "Dear ";
+
+            
+
+            SAVE(word_doc, word_app);
+        }
+        private void SAVE(Word.Document word_doc, Word.Application word_app)
+        {
+            var saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.ShowDialog();
+
+            string filename = saveFileDialog1.FileName;
+
+            try
+            {
+                word_doc.SaveAs(filename);
+                MessageBox.Show("файл сохранен");
+                word_app.Visible = true;
+            }
+            catch { MessageBox.Show("произошла ошибка"); }
+        }
+
+        private string OPEN()
+        {
+            var openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ShowDialog();
+            
+            string filename = openFileDialog1.FileName;
+            return filename;
+        }
+
+        private void ReplaceWordStub(string stubToReplace, string text, Word.Document wordDocument)
+        {
+            var range = wordDocument.Content;
+            range.Find.ClearFormatting();
+            range.Find.Execute(FindText: stubToReplace, ReplaceWith: text);
+
+        }
+
+
+        }
+
+    
+   
+
+    
+}
